760af0bafdefacbef385bcc75413bf1e20f7cd5c
Kulischiano <32580330+Kulischiano@users.noreply.github.com>
2018-03-16 22:48:11 +0300

    Add files via upload

---
+++ exersise_3.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main(void)
+{ printf("Разность %i and %i is %i", 87, 15, 87-15);
+}
---
+++ exersise_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+int sum;
+
+// Подсчитать результат
+sum = 25 + 37 - 19;
+
+// Отобразить результат
+printf("Ответ %i\n", sum);
+return 0;
+}
