6218ca06c3be73fe0cbc06cdcb3d3ec637a3afa7
Kulischiano <32580330+Kulischiano@users.noreply.github.com>
2018-03-20 00:32:22 +0300

    исправлено

---
+++ 4exersise_8.c
@@ -0,0 +1,18 @@
+ #include <stdio.h> 
+
+int main(void) 
+{ 
+int i = 365; 
+int j = 7; 
+printf("Следующая переменная %i and %i это %i\n", i, j, i + j - i % j); 
+
+int k = 12258; 
+int l = 23; 
+printf("Следующая переменная %i and %i это %i\n", k, l, k + l - k % l); 
+
+int m = 996; 
+int n = 4; 
+printf("Следующая переменная %i and %i это %i\n", m, n, m + n - m % n); 
+
+return 0; 
+}
