6dc4febf6050219c93a5f704148b1133502a8b0d
Kulischiano <32580330+Kulischiano@users.noreply.github.com>
2018-04-07 11:11:27 +0300

    Update BSEARCH.c

--- BSEARCH.c
+++ BSEARCH.c
@@ -3,26 +3,26 @@ int main(void)
 {
 	int array[10] = { 2, 4, 6, 8, 10, 12, 16, 18, 20, 22 };
 	int srchVal = 6;
-	int midle;
+	int middle;
 	int size = sizeof(array) / 6;
 	int low = 0;
 	int high = size;
 	while (low <= high)
 	{
-		midle = (low + high) / 2;
+		middle = (low + high) / 2;
 		if (srchVal == array[midle])
 		{
 			printf("Номер элемента, который ты искал: %i\n", midle + 1);
 			break;
 
 		}
-		else if (srchVal > array[midle])
+		else if (srchVal > array[middle])
 		{
-			low = midle + 1;
+			low = middle + 1;
 		}
 		else
 		{
-			high = midle - 1;
+			high = middle - 1;
 
 		}
 
